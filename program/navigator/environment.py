5d15776f1785cac8d9d20b034a1362559c02dfd3
toth135 <toth.peter618@gmail.com>
2020-11-15 21:43:16 +0100

    is_in_wall and add_wall method added

--- program/navigator/environment.py
+++ program/navigator/environment.py
@@ -14,10 +14,18 @@ class Environment:
     def walls(self, walls):
         self._walls = walls
 
+    # def add_wall(self, x1, y1, x2, y2):
+    #     self._walls.append((x1, y1, x2, y2))
+
     def is_in_wall(self, position):
         """
         Check that the given point is in wall.
         :param position: tuple of (x, y) coordinates
         :return: True, when the point is in the wall, else False
         """
-        pass
+        x, y = position
+
+        for wall in self.walls:
+            if wall[0] <= x <= wall[2] and wall[1] <= y <= wall[3]:
+                return True
+        return False
--- program/navigator/navigator.py
+++ program/navigator/navigator.py
@@ -1,10 +1,16 @@
+import python.program.navigator.environment as env
+
+
 class Navigator:
     """
     Navigator for four-wheeled vehicles in a simplified environment
     """
     
     def __init__(self):
-        pass
+        self.environment = []
+
+    def add_wall(self, x1, y1, x2, y2):
+        self.environment.append((x1, y1, x2, y2))
 
     def find_path(self, vehicle_start, vehicle_finish, environment):
         """
@@ -14,5 +20,10 @@ class Navigator:
         :param environment: the map
         :return: list of angles
         """
+
+        start_position = vehicle_start
+        end_position = vehicle_finish
+        environment = environment
+
         # NOTE: The step size is matter!
         return []
